be4b6fc75dcebf857d65ffc4fe9a380c491f3bfc
Max Lima <max.lima2@gmail.com>
2020-01-07 22:34:44 -0300

    Função para inserir issues no database

--- lib/json_to_sql.py
+++ lib/json_to_sql.py
@@ -25,6 +25,26 @@ def insertCommitsCommand(keys, values, json_file):
     return sql
 
 
+def insertIssuesCommand(keys, values, json_file):
+    sql = "INSERT INTO issues ("
+    for i in range(len(keys)):
+        atribute_name = keys[i].lower().replace("-", "_")
+        if atribute_name == "user":
+            atribute_name = "user_info"
+        if i == len(keys) - 1:
+            sql += f"{atribute_name}"
+        else:
+            sql += f"{atribute_name}, "
+    sql += ") VALUES (\n"
+    for i in range(len(keys)):
+        if i == len(keys) - 1:
+            sql += "%s);"
+        else:
+            sql += "%s, "
+
+    return sql
+
+
 def _createTable(tables, keys, attributes, category, connection, cursor):
     table = 'repositorys' if category == 'repository' else category
     if table in tables:
