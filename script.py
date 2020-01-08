dd9b95d6708bff043f13de153fa4235e8cee0961
Max Lima <max.lima2@gmail.com>
2020-01-07 22:11:28 -0300

    Checar existencia do repositorio no database

--- script.py
+++ script.py
@@ -1,3 +1,31 @@
+def checkRepoExists(user_owner, repo_name, cursor):
+    sql = f"""
+        SELECT EXISTS (
+            SELECT *
+            FROM   information_schema.tables
+            WHERE  table_schema = 'serg'
+            AND    table_name = 'repositorys'
+        );
+    """
+
+    cursor.execute(sql)
+    tables = cursor.fetchall()
+
+    if tables[0][0]:
+        sql = f"""
+        SELECT
+            *
+        FROM
+            repositorys
+        WHERE
+            owner = {user_owner} AND
+            repository = {repo_name}
+        ;"""
+        cursor.execute(sql)
+        return cursor.fetchall()
+    return None
+
+
 def run(owner, repository):
     data_base_url = os.environ.get("DATABASE_URL")
     conn = psycopg2.connect(data_base_url, sslmode='require')
@@ -15,5 +43,7 @@ def run(owner, repository):
             index += 1
         else:
             break
+
+    repositorys = checkRepoExists(owner, repository, cursor)
 
     conn.close()
